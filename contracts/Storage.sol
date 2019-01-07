4e4907357aab47fd6fbb766589dfd8d8c3e25254
Rajat Shamra <rajat.sharma@xooa.com>
2019-01-07 17:03:22 +0530

    version upgrade

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -1,12 +1,12 @@
-pragma solidity ^0.4.8;
+pragma solidity ^0.4.23;
 
 contract Storage {
 
     uint256 storedData;
-    function set(uint256 data) {
+    function set(uint256 data) public {
         storedData = data;
     }
-    function get() view returns (uint256) {
+    function get() public  view returns (uint256) {
         return storedData;
     }
 }
